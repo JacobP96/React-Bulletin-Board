a1aa9664ee4080f6bf8b1c3d671a5983abccb5eb
Jacob Persson <jacobpersson96@gmail.com>
2022-01-18 19:36:31 +0100

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,18 +6,18 @@ function App() {
     {
       question: "Vad är tre fördelar med att använda React?",
       answer:
-        "Komponenterna i react gör att kod kan delas upp i mindre enheter,som gör att koden blir lättare arr återanvända,utan större tekniska svårigheter. React är välanvänt avf större företag så som Facebook,Microsoft och Paypal bland andra. De flesta buggarna är lösta och det finns standader för hur man hanterar exempelvis, data-hantering eller autentisering på bästa sätt.  "
+        "Att använda sig av komponenter i React gör att kod kan delas upp i mindre enheter. Detta gör att koden blir lättare att återanvända utan större tekniska svårigheter. React är välanvänt av större företag så som Facebook,Microsoft och Paypal bland andra. De flesta buggar i React är lösta och det finns standader för hur man hanterar exempelvis data-hantering eller autentisering på bästa sätt.  "
     },
     {
       question:
         "Vad betyder Single Page Application och vad skiljer det från en “vanlig” webbplats?",
       answer:
-        "Single Page Application uppdaterar bara informationen på hemsidan som användaren efterfrågar, medans på en `vanlig` hemsida uppdateras informationen på hela sidan, oavsett vilken information du vill få fram. "
+        "Single Page Application laddar hela sidan en gång och efterföljande klick till nya 'sidor' resulterar i minimal trafik. Vilket innebär att bara den efterfrågade informationen kommer att uppdateras på hemsidan, vilket ökar webbplatsens prestanda avsevärt. På en 'vanlig' hemsida uppdateras informationen på hela sidan, oavsett vilken information du vill få fram. "
     },
     {
       question: "Nämn tre skillnader mellan React och Angular.",
       answer:
-        "React fungerar som ett bibliotek medans Angular är mer ett ramverk. Data-bindnings är också en stor skillnad då React fungerar som one-way och Angylar fungerar two-way. I React jobbar man med Javascript och när man jobbar i Angular använder man sig av Microsoft's programmeringsspråk"
+        "React fungerar som ett bibliotek medan Angular är mer ett ramverk. Data-bindnings är också en stor skillnad då React fungerar som one-way och Angylar fungerar two-way. I React jobbar man med Javascript och när man jobbar i Angular använder man sig av Microsoft's programmeringsspråk"
     }
   ];
   return (
